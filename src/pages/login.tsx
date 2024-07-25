dc35fd7bfc52a3f7a75070cb83ae8af195659091
kevinzou54 <kevinzou54@gmail.com>
2024-07-25 15:24:36 -0700

    added authentication thru google

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,14 @@
+import {auth, provider} from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+
 export const Login = () => {
-    return <div>Login Page</div>
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+    };
+
+    return <div>
+        <p>Sign in with Google to Continue</p>
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
 }
